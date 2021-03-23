2ecb4b41646a6ee098e156e2c97d35c067cba9c8
Adnan Sarker <adnansarker92@gmail.com>
2021-03-23 22:07:17 +0600

    add progressView functionality and update code

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -13,24 +13,32 @@ let hardTime = 12
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     var timer: Timer?
     
     let eggTime = ["Soft": softTime, "Medium" : mediumTime, "Hard" : hardTime]
     
-    var timeLeft = 0
+    var timeLeft = 0;
+    var timeFlag = 0;
     
-    @IBOutlet weak var titleLabel: UILabel!
+
     @IBAction func buttonPressed(_ sender: UIButton) {
         
+        timer?.invalidate()
         let hardness = sender.currentTitle!
-        timeLeft = eggTime[hardness]! * 60;
-        
+        timeLeft = eggTime[hardness]! * 1;
+        timeFlag = timeLeft
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ttimer), userInfo: nil, repeats: true)
     
     }
+
     
     @objc func ttimer (){
+        
         timeLeft -= 1
+        //var x = timeFlag - timeLeft
         titleLabel.text = " \(timeLeft) seconds left"
         
         if timeLeft<=0 {
@@ -38,6 +46,10 @@ class ViewController: UIViewController {
             
             timer?.invalidate()
             timer = nil
+            progressView.progress = 0
+        }
+        else {
+            progressView.progress = Float(timeFlag - timeLeft)/Float(timeFlag)
         }
     }
     
